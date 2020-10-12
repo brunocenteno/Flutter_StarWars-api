8d167b5ca52ee001dc4cd0a860d685b7af2f18fe
Bruno Centeno <brunocenteno@outlook.com>
2020-10-12 13:03:41 -0500

    Initial commit
    Add the entire app

---
+++ README.md
@@ -0,0 +1,13 @@
+# call_swapi
+
+A new Flutter application.
+
+Where we call on the Star Wars API 
+
+to retrieve names and descriptions of differents objects
+
+of the Star Wars Universe
+
+The flutter app shows how to call the API and
+
+use the widgets to show the information
---
+++ lib/main.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(MaterialApp(
+    home: StarWarsData(),
+  ));
+}
+class StarWarsData extends StatefulWidget {
+  @override
+  StarWarsState createState() => StarWarsState();
+}
+
+//Getting the json and putting it into the variables
+class StarWarsState extends State<StarWarsData> {
+  final String url = "https://swapi.co/api/starships";
+  List data;
+
+  Future<String> getSWData() async {
+    var res =
+    await http.get(Uri.parse(url), headers: {"Accept": "application/json"});
+
+    setState(() {
+      var resBody = json.decode(res.body);
+      data = resBody["results"];
+    });
+
+    return "Success";
+  }
+//-----------------------------------------//
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Star Wars Starships"),
+        backgroundColor: Colors.deepPurpleAccent,
+      ),
+      body: ListView.builder(
+        itemCount: data == null ? 0 : data.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            child: Center(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  FlatButton(//TODO add new view to the starwars ships
+                    //onPressed: nothing(),
+                    child: Card(
+                      child: Container(
+                          padding: EdgeInsets.all(15.0),
+                          child: Column(
+                            children: <Widget>[
+                              Text(data[index]["name"],
+                                  style: TextStyle(
+                                      fontSize: 18.0, color: Colors.black87)),
+                              Text(data[index]["model"],
+                                  style: TextStyle(
+                                      fontSize: 18.0, color: Colors.red)),
+                              Text(data[index]["cargo_capacity"],
+                                  style: TextStyle(
+                                      fontSize: 18.0, color: Colors.black87)),
+                            ],
+                          )
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+  super.initState();
+  this.getSWData();
+  }
+}
